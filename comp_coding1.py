b88f40771bbd907f4b7d1d41f0691e41a56445f3
Kavi16-02 <kavisharma620@gmail.com>
2021-06-06 17:04:21 +0530

    k

--- comp_coding1.py
+++ comp_coding1.py
@@ -1,7 +1,10 @@
 #program to find no. of a in string wrt the n
+<<<<<<< HEAD
 s = input()
 n = int(input())
 
+=======
+>>>>>>> 7c0ad4e44f52beb5c5d66b9810cd27f97d8a6554
 def rep(s,n):
     r=0
     l=len(s)
@@ -13,5 +16,9 @@ def rep(s,n):
         if s[i]=='a':
             r+=1
     return r
+<<<<<<< HEAD
 k=rep(s,n)
+=======
+k=rep('kvai',10)
+>>>>>>> 7c0ad4e44f52beb5c5d66b9810cd27f97d8a6554
 print(k)
