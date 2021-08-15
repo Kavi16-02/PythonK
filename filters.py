8ee163e6a2d52ed65e5915976472d57baca3bba9
Kavi16-02 <kavisharma620@gmail.com>
2021-08-15 18:32:55 +0530

    msg

--- filters.py
+++ filters.py
@@ -9,3 +9,12 @@ even = list(filter(is_even,nums))
 print(even)
 odd=list(filter(is_odd,nums))
 print(odd)
+
+
+'''or using lambda functions'''
+
+nums=[1,2,3,4,5,67,8,8,9]
+even = list(filter(lambda n: n%2==0,nums))
+print(even)
+odd=list(filter(lambda n:n%2!=0,nums))
+print(odd)
